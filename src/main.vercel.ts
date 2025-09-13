16112fd0ecd409b48fdfb2d7bc2524ee32c7e41b
Acrybab <khangtntce171659@fpt.edu.vn>
2025-09-14 00:06:17 +0700

    a

--- src/main.vercel.ts
+++ src/main.vercel.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
